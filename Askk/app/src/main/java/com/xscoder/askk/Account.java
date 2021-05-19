a3010aac202821ce85059cc020f4d7626c8db108
raihan <ryihan55@gmail.com>
2021-05-20 05:56:48 +0600

    Update Account.java

--- Askk/app/src/main/java/com/xscoder/askk/Account.java
+++ Askk/app/src/main/java/com/xscoder/askk/Account.java
@@ -22,3 +22,6 @@ import android.content.Intent;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import static com.xscoder.askk.XServerSDK.popBold;
+import static com.xscoder.askk.XServerSDK.popRegular;
+import static com.xscoder.askk.XServerSDK.roundLargeNumber;
