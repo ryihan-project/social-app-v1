289fe402d8924cd8a1e7c8428bf8828a4b9366a0
raihan <ryihan55@gmail.com>
2021-05-20 05:56:22 +0600

    Create Account.java

---
+++ Askk/app/src/main/java/com/xscoder/askk/Account.java
@@ -0,0 +1,15 @@
+package com.xscoder.askk;
+
+/**
+
+ Askk [XServer]
+
+ © XScoder 2020
+ All Rights reserved
+
+ * IMPORTANT *
+ RE-SELLING THIS SOURCE CODE TO ANY ONLINE MARKETPLACE
+ IS A SERIOUS COPYRIGHT INFRINGEMENT, AND YOU WILL BE
+ LEGALLY PROSECUTED
+
+ **/
