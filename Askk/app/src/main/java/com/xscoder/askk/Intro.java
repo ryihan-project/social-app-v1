262c32d5f8649a9e5a5f736c383fba27bd3dc2c0
raihan <ryihan55@gmail.com>
2021-05-29 22:38:27 +0600

    Update Intro.java

--- Askk/app/src/main/java/com/xscoder/askk/Intro.java
+++ Askk/app/src/main/java/com/xscoder/askk/Intro.java
@@ -14,6 +14,20 @@ package com.xscoder.askk;
 
  **/
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 public class Intro extends AppCompatActivity {
 
 
