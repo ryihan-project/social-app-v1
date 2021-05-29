219787ed2b164872354e999e20b44f9cda3fdbb8
raihan <ryihan55@gmail.com>
2021-05-29 22:40:32 +0600

    Update Login.java

--- Askk/app/src/main/java/com/xscoder/askk/Login.java
+++ Askk/app/src/main/java/com/xscoder/askk/Login.java
@@ -14,6 +14,23 @@ package com.xscoder.askk;
 
  **/
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Objects;
 public class Login extends AppCompatActivity {
 
 
